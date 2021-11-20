338d810e0123f73b34899da787fbb8d137e1657a
Lucas Malheiros <lucmalheiros@gmail.com>
2021-11-20 17:19:52 -0300

    Criação páginas
    Páginas criadas:
    Desenvolvedor, Tecnologias e Configurações.
    Adição do Body Parser
    Criação de pseudo login para testes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Programação Web 2'});
 });
 
+router.get('/login', function(req, res, next){
+  console.log("Before",process.env.LOGGEDIN);
+  process.env.LOGGEDIN=true;
+  console.log("After",process.env.LOGGEDIN);
+  res.redirect('/')
+});
+router.get('/logout', function(req, res, next){
+  console.log("Before",process.env.LOGGEDIN);
+  process.env.LOGGEDIN=false;
+  console.log("After",process.env.LOGGEDIN);
+  res.redirect('/')
+});
+
 module.exports = router;
